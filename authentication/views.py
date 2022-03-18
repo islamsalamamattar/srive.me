292ad9102611399599f5f89125cdd19d53e9bdc4
islamsalamamattar <islam-salama@hotmail.com>
2022-03-18 16:28:53 +0200

    registration view - login after creation of user

--- authentication/views.py
+++ authentication/views.py
@@ -47,7 +47,7 @@ def register_user(request):
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
-
+            login(request, user)
             msg     = 'User created - please <a href="/login">login</a>.'
             success = True
             
