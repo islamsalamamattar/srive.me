4db8bf3525a423bab0f6cf97febbfc8a18220710
islamsalamamattar <islam-salama@hotmail.com>
2022-03-27 00:15:06 +0200

    starting REST api configuration

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from expenses import models
-from serializer import ExpenseSerializer
+from .serializer import ExpenseSerializer
 
 
 @api_view('GET')
 def ExpenseIndexApi(request):
     expenses = models.Expense.objects.all()
-    serliazer = ExpenseSerializer(expenses, many=True)
-    return Response(serliazer.data)
+    serializer = ExpenseSerializer(expenses, many=True)
+    return Response(serializer.data)
