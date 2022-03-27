10006b0a50bb47d143a5a770d3d67cdeaba98e36
islamsalamamattar <islam-salama@hotmail.com>
2022-03-27 21:27:11 +0200

    adding api login

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from api import views
 
 urlpatterns = [
     path("api/", views.ExpenseIndexApi, name="expenses_api"),
+    path('api/login/', views.LoginView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -2,16 +2,34 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework import views
+
+from django.contrib.auth.decorators import login_required
+
 from expenses import models as exp
-from .serializer import ExpenseSerializer
+from . import serializers
+from .serializer import ExpenseSerializer, LoginSerializer
 
 
 
 
 @api_view(['GET'])
-@authentication_classes([SessionAuthentication, BasicAuthentication])
-@permission_classes([IsAuthenticated])
+@login_required(login_url='api/login/')
 def ExpenseIndexApi(request):
     expenses = exp.Expense.objects.all().order_by('-date', '-id')
     serializer = ExpenseSerializer(expenses, many=True)
     return Response(serializer.data)
+
+
+class LoginView(views.APIView):
+    # This view should be accessible also for unauthenticated users.
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = serializers.LoginSerializer(data=self.request.data,
+            context={ 'request': self.request })
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response(None, status=status.HTTP_202_ACCEPTED)
