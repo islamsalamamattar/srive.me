e0c2d8550de1126801c04d0b1689db32ccbd939c
islamsalamamattar <islam-salama@hotmail.com>
2022-04-04 23:11:23 +0200

    fixing login redirect

--- expenses/urls.py
+++ expenses/urls.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 
 
 urlpatterns = [
-    #path('', lambda req: redirect('expenses')),
+    path('', lambda req: redirect('expenses')),
     path('expenses/', views.ExpenseIndex, name="expenses"),
     path('expenses/analytics', views.ExpenseAnalytics, name='analytics'),
     path('expenses/delete_expense/<slug:expense_id>', views.DeleteExpense, name='delete_expense'),
