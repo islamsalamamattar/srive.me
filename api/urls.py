a72256fb67584382978de205c1c023dec5d70cdd
islamsalamamattar <islam-salama@hotmail.com>
2022-04-07 01:20:21 +0200

    api test 1 million, Response and no suth

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    #path("api/", views.ExpenseIndexApi, name="expenses_api"),
+    path("api/", views.ExpenseIndexApi, name="expenses_api"),
     path("api/test", views.ExpensesApi, name="api"),
     path('api/login/', views.LoginView.as_view()),
 ]
