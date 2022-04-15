baad5659943fc94b01671c68817ac9247fbcca0c
islamsalamamattar <islam-salama@hotmail.com>
2022-04-15 14:31:07 +0200

    update url

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from api import views
 
 urlpatterns = [
     path("api/", views.ExpenseIndexApi, name="expenses_api"),
-    path("apitest", views.ExpensesApi, name="api"),
+    path("apitest/", views.ExpensesApi, name="api"),
     path('api/login/', views.LoginView.as_view()),
 ]
