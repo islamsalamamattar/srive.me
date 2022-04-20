bcb34bf5a3a92aa3a03cb7e149a54996a36805be
islamsalamamattar <islam-salama@hotmail.com>
2022-04-20 03:25:18 +0200

    adding autopay subscriptions and manual list, activated autopay at expensesIndex view

--- expenses/models.py
+++ expenses/models.py
@@ -151,5 +151,5 @@ class Subscription(models.Model):
 
     objects = SubscriptionManager()
     def __str__(self):
-        return f"{self.name} , {self.amount}"
+        return f"{self.note} , {self.amount}"
 
