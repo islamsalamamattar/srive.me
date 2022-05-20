b99a2f6f184a87a669aa59c0299cd7e6f267966d
islamsalamamattar <islam-salama@hotmail.com>
2022-05-20 19:10:28 +0200

    first view for Todo, test contxt on row html

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 # Todo Categories
 class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=25)
     color = models.CharField(max_length= 64, null=True, blank=True)
     icon = models.CharField(max_length= 64, null=True, blank=True)
@@ -12,6 +14,19 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
 # Todo
+class TodoManager(models.Manager):
+    def not_completed(self, user):
+        list = self.filter(user=user, completed=False).order_by('-category', '-added_date')
+        not_completed = {}
+        for todo in list:
+            if not not_completed[todo.category]:
+                not_completed[todo.category] = []
+            not_completed[todo.category].append(todo)
+        return not_completed
+    def all_todos(self, user):
+        return self.filter(user=user)
+
+
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_added = models.DateField(auto_now=False, auto_now_add=True, )
@@ -19,5 +34,6 @@ class Todo(models.Model):
     task = models.CharField(max_length=140)
     completed = models.BooleanField(default=False)
     date_updated = models.DateField(auto_now=True)
+    objects = TodoManager()
     def __str__(self):
         return f"{self.category} , {self.item}"
