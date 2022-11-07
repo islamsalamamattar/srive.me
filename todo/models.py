bae9c2bf57d32c655a7cc4f77ea7bb667845b423
islamsalamamattar <islam-salama@hotmail.com>
2022-11-07 14:20:00 +0200

    todo Modelform

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Q
+from django.forms import ModelForm
 
 # Todo Categories
 class Category(models.Model):
